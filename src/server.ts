34bb5bc49364c23485aa2af440e395ebe600400d
alexanderroshchin <alexander.roshchin85@gmail.com>
2023-03-30 19:12:50 +0300

    Implemented getting boards

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import * as usersController from './controllers/users';
+import * as boardsController from './controllers/boards';
+
 import authMiddleware from './middlewares/auth';
 
 const app = express();
@@ -21,9 +23,18 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.set('toJSON', {
+  virtuals: true,
+  transform: (_, converted) => {
+    delete converted._id;
+  },
+});
+
 app.post('/api/users/register', usersController.register);
 app.post('/api/users/login', usersController.login);
 app.get('/api/user', authMiddleware, usersController.currentUser);
+
+app.get('/api/boards', authMiddleware, boardsController.getBoards);
 
 app.get('/', (req, res) => {
   res.send('API is UP');
